1cbbbc1ff3db3f673e1c3c1317f65cb6fdeb1b4a
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-09-04 14:09:31 +0200

    debug: setup debug files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import { LogLevelEnum, RainbowSDK } from 'rainbow-web-sdk';
 import config from '../config.json';
-
 class TestRainbowSDK {
-    protected rainbowSDK: RainbowSDK;
-
     constructor() {
-        this.test()
+        this.test();
     }
-
-    public async test(): Promise<void> {
-        console.log("test")
+    async test() {
+        console.log("test");
         this.rainbowSDK = RainbowSDK.create({
-            appConfig: { 
-                server: config.RAINBOW_SERVER || 'demo.openrainbow.org', 
+            appConfig: {
+                server: config.RAINBOW_SERVER || 'demo.openrainbow.org',
                 applicationId: config.RAINBOW_APP_ID || '',
                 secretKey: config.RAINBOW_SECRET_KEY || ''
             },
@@ -20,9 +16,8 @@ class TestRainbowSDK {
             autoLogin: true,
             logLevel: LogLevelEnum.WARNING
         });
-
         // const sdk: RainbowSDK = RainbowSDK.getInstance();
     }
 }
-
 const testRainbowSDK = new TestRainbowSDK();
+//# sourceMappingURL=index.js.map
