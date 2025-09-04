634f1fc8ae12f286aeaa4ab5d65e30b6c22d00da
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-09-04 13:01:13 +0200

    feat: main template

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import {RainbowSDK} from 'rainbow-web-sdk';
+import {ConnectedUser, LogLevelEnum, RainbowSDK} from 'rainbow-web-sdk';
 import config from '../config.json';
 
 class TestRainbowSDK {
     protected rainbowSDK: RainbowSDK;
 
+    constructor() {
+        this.test()
+    }
+
+    public async test(): Promise<void> {
+        console.log("test")
+        this.rainbowSDK = RainbowSDK.create({
+            appConfig: { 
+                server: config.RAINBOW_SERVER || 'demo.openrainbow.org', 
+                applicationId: config.RAINBOW_APP_ID || '',
+                secretKey: config.RAINBOW_SECRET_KEY || ''
+            },
+            plugins: [],
+            autoLogin: true,
+            logLevel: LogLevelEnum.WARNING
+        });
+
+        // const sdk: RainbowSDK = RainbowSDK.getInstance();
+    }
 }
 
+const testRainbowSDK = new TestRainbowSDK();
