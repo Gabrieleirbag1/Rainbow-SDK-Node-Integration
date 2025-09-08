64a9397cf406301760a72e9f727ae22dd739b319
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-09-08 15:18:32 +0200

    refractor(feat): add login methods and made a class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { JSDOM } from 'jsdom';
-import WebSocket from 'ws';
+import { JSDOM } from "jsdom";
+import WebSocket from "ws";
 import { LocalStorage } from "node-localstorage";
-import configuration from '../config.json';
-import { RainbowSDK } from 'rainbow-web-sdk/lib';
+import configuration from "../config.json";
+import {
+  ConnectedUser,
+  ConnectionState,
+  RainbowSDK,
+  RBEvent,
+} from "rainbow-web-sdk/lib";
 
 // Create JSDOM and expose globals BEFORE importing rainbow-web-sdk
-const DOM = new JSDOM(`<!DOCTYPE html><html><body><p>Placeholder</p></body></html>`, {
-    url: 'http://localhost'
-});
+const DOM = new JSDOM(
+  `<!DOCTYPE html><html><body><p>Placeholder</p></body></html>`,
+  {
+    url: "http://localhost",
+  }
+);
 // console.log(DOM.window.document.querySelector("p")?.textContent);
 
 // expose DOM globals
@@ -25,32 +33,64 @@ declare const WebSocket;
 Event = DOM.window.Event;
 EventTarget = DOM.window.EventTarget;
 
-globalThis.config = {}
+globalThis.config = {};
 
-async function main() {
-    // 2) Import the SDK only after the environment is ready
-    const { RainbowSDK, LogLevelEnum } = await import('rainbow-web-sdk');
+class NodeRainbowSDK {
+  protected rainbowSDK: RainbowSDK;
 
+  public async main() {
     console.log("init");
-
-    // const sdk = RainbowSDK.getInstance();
+    // Import the SDK objects only after the environment is ready
+    const { RainbowSDK, LogLevelEnum, ConnectionServiceEvents } = await import(
+      "rainbow-web-sdk"
+    );
 
     debugger;
-    const sdk: RainbowSDK = RainbowSDK.create({
+    this.rainbowSDK = RainbowSDK.create({
       appConfig: {
-        server: configuration.RAINBOW_SERVER || 'demo.openrainbow.org',
-        applicationId: configuration.RAINBOW_APP_ID || '',
-        secretKey: configuration.RAINBOW_SECRET_KEY || ''
+        server: configuration.RAINBOW_SERVER || "demo.openrainbow.org",
+        applicationId: configuration.RAINBOW_APP_ID || "",
+        secretKey: configuration.RAINBOW_SECRET_KEY || "",
       },
       plugins: [],
       autoLogin: true,
-      logLevel: LogLevelEnum.DEBUG
+      logLevel: LogLevelEnum.DEBUG,
     });
+
+    this.rainbowSDK.connectionService.subscribe(
+      (event: RBEvent) => this.connectionStateChangeHandler(event),
+      ConnectionServiceEvents.RAINBOW_ON_CONNECTION_STATE_CHANGE
+    );
+
+    let userConnected: ConnectedUser = await this.rainbowSDK.start();
+    const user = configuration.RAINBOW_USER || "";
+    const pwd = configuration.RAINBOW_PASSWORD || "";
+
+    if (!userConnected) {
+      try {
+        userConnected = await this.rainbowSDK.connectionService.logon(
+          user,
+          pwd,
+          true
+        );
+      } catch (error: any) {
+        console.error(`[testAppli] ${error.message}`);
+        return;
+      }
+    }
+  }
+
+  private connectionStateChangeHandler(event: RBEvent): void {
+    const connectionState: ConnectionState = event.data;
+    console.info(
+      `[testAppli] onConnectionStateChange ${connectionState.state}`
+    );
+  }
 
 }
 
-console.log("ot");
+const sdk = new NodeRainbowSDK();
 
-main().catch((error) => {
-    console.error("Error initializing Rainbow SDK:", error);
+sdk.main().catch((error) => {
+  console.error("Error initializing Rainbow SDK:", error);
 });
